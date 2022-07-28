569fc3d776a13b13be217189eefb48afc8f3d623
Mohamad Moein HajEsmaeili <mohamadmoein.h@gmail.com>
2022-07-28 23:57:06 +0430

    Invoke rust function from js

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import type { Component } from 'solid-js';
+import { invoke } from '@tauri-apps/api';
 
 const App: Component = () => {
+
+  invoke('greet', { name: 'Mohamad Moein' }).then(res => console.log(res))
+
   return (
-    <h1 class="font-bold text-3xl text-green-700 text-center py-20">Hello World! Moein from tauri app.</h1>
+    <div>
+      <h1 class="font-bold text-3xl text-green-700 text-center py-20">Hello World!</h1>
+    </div>
   );
 };
 
